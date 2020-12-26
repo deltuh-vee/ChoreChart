6e7c6148a0a8dfc996eb6263a3fde02617dce26a
unknown <739curli@gmail.com>
2020-12-26 13:36:14 -0800

    Created add chore component

---
+++ ClientApp/src/app/add-chore/add-chore.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-chore',
+  templateUrl: './add-chore.component.html',
+  styleUrls: ['./add-chore.component.css']
+})
+export class AddChoreComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
